93df912286656f2b41fb9a5210e3bfdc34a6f324
Adam Rabenstein <adam.rabenstein@gmail.com>
2013-12-30 18:03:20 -0800

    added stuff to be used in future

--- functions/get_system_info.ps1
+++ functions/get_system_info.ps1
@@ -1,7 +1,10 @@
 <#
 .Version
-    0.1
-    Created script, formated it out.
+0.1
+Created script, formated it out.
+0.2
+Added basics for system, processor, os.
+0.3
 
 .Synopsis
    Created by Adam Rabenstein on 12-24-2013.   
@@ -10,6 +13,7 @@
    get_system_info $computers
 
 .Todo
+make an itoration function that itorates through an array and then i'd have to combine things later.
     
 #>
 
@@ -29,6 +33,8 @@ Param(
         $processor = gwmi win32_processor -ComputerName $c #get processor from wmi
         $osinfo = gwmi win32_operatingsystem -ComputerName $c #os from wmi
         $printers = Get-WmiObject -Class win32_printer -ComputerName $c
+        $profiles = Get-WmiObject -Class win32_userprofile -ComputerName $c
+        $drives = Get-WmiObject -Class win32_mappedlogicaldisk -ComputerName $c
 
         $view = New-Object PsObject -Property @{ #new object to itorate through and allow easier formatting, etc
             SystemName = [String]$system.Name
@@ -41,6 +47,11 @@ Param(
             OsName = [String]$osinfo.Caption
             OsServicePack = [String]$osinfo.CSDVersion
             OsArch = [String]$osinfo.OSArchitecture
+            #PrinterPort = [String]$printers.PortName
+            #PrinterShareName = [String]$printers.ShareName
+            #PrinterServer = [String]$printers.ServerName
+            #PrinterLocal = [String]$printers.Local
+            
         }
         $results += $view
     }
