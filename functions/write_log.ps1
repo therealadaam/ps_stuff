dd7a21d6c08457a59e800b123218403e2e15464c
Adam Rabenstein <adam.rabenstein@gmail.com>
2014-01-06 12:48:29 -0800

    added write_log function

---
+++ functions/write_log.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.Synopsis
+   Created by Adam Rabenstein on 1-6-2014.
+   Writes output to a log with the scriptname.
+  
+.EXAMPLE
+   write_log "Stuff to write"
+#>
+
+function write_log {
+Param($toWrite)
+    $log = $MyInvocation.ScriptName + ".log"
+    $time = Get-Date -Format 'dd-MM-yy_HH:MM::ss'
+    Add-Content -Value $time -Path $log
+    Add-Content -Value $toWrite -Path $log
+}
+write_log "Testing"
