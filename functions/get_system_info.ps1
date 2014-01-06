ea24703aa0e4b56101bb9cdc937c0d3069ffc65c
Adam Rabenstein <adam.rabenstein@gmail.com>
2014-01-06 10:25:49 -0800

    Finished network and drives. All exporting to csv. I also cleaned a bunch of stuff up and removed spaces and old code

--- functions/get_system_info.ps1
+++ functions/get_system_info.ps1
@@ -28,37 +28,12 @@ Param(
     }
     return $obj
 }
-$printers = Get-WmiObject -Class win32_printer
-#$printerRes = user_printers($printers)
-
-function user_printers_old {
-Param(
-        [Parameter(Mandatory=$true, 
-                    ValueFromPipeline=$false,                    
-                    Position=0)]        
-	    $ary
-    )
-    $res = @()
-    foreach ($i in $ary) {
-        $temp = New-Object psobject -Property @{
-            PrinterName = [String]$i.name
-            PrinterPort = [String]$i.PortName
-            PrinterShareName = [String]$i.ShareName
-            PrinterServer = [String]$i.ServerName
-            PrinterLocal = [String]$i.Local
-        }
-        $res += $temp
-    }
-    return $res
-}
 
 function user_profiles {
 Param(
         $ary,
         $obj
     )
-    
-
     #main part
     $num = 0
     foreach ($i in $ary) {
@@ -93,149 +68,72 @@ Param(
         $obj | Add-Member -Name "Profile_$($num)_RoamingPath" -Value "$($i.RoamingPath)" -MemberType NoteProperty
         $obj | Add-Member -Name "Profile_$($num)_LastUse" -Value "$($lut)" -MemberType NoteProperty
         $obj | Add-Member -Name "Profile_$($num)_UserName" -Value "$($tmpUsr.Value)" -MemberType NoteProperty
-
-        
-
         $num++
     }
-    <# Old stuff
-    $temp = New-Object psobject -Property @{
-            ProfileSID = [String]$i.SID
-            ProfileStatus = $i.Status
-            ProfileLocalPath = [String]$i.LocalPath
-            ProfileRamingPath = [String]$i.RoamingPath
-            ProfileLastUse = [String]$lut
-            ProfileUsername = [String]$tmpUsr.Value
-        } 
-    #>
-
-
-    #Switch for ProfileStatus
-    
-
     return $obj
 }
 
-function user_profiles_old {
-Param(
-        [Parameter(Mandatory=$true, 
-                    ValueFromPipeline=$false,                    
-                    Position=0)]        
-	    $ary
-    )
-    $res = @()
-    foreach ($i in $ary) {
-        if ($i.special) { continue } #Skip special accounts, networkservice,etc.
-        
-        #translate SID to username
-        try {
-            $tmpSID = New-Object System.Security.Principal.SecurityIdentifier($i.sid)
-            $tmpUsr = $tmpSID.Translate([System.Security.Principal.NTAccount])
-        }
-        catch {
-            $tmpUsr = New-Object psobject -Property @{Value=""}
-            $tmpUsr.Value = [String]$i.sid
-        }
-
-        #ProfileTime Conversion
-        $lut = ([WMI]"").Converttodatetime($i.lastusetime)
-        [String]$lut = $lut |Get-Date -Format 'MM-dd-yyyy'
-        
-
-        $temp = New-Object psobject -Property @{
-            ProfileSID = [String]$i.SID
-            ProfileStatus = $i.Status
-            ProfileLocalPath = [String]$i.LocalPath
-            ProfileRamingPath = [String]$i.RoamingPath
-            ProfileLastUse = [String]$lut
-            ProfileUsername = [String]$tmpUsr.Value
-        }        
-        #Switch for ProfileStatus
-        switch ($temp.ProfileStatus) {
-            1 {$temp.ProfileStatus = [string]"Temporary"}
-            2 {$temp.ProfileStatus = [string]"Roaming"}
-            4 {$temp.ProfileStatus = [string]"Mandatory"}
-            8 {$temp.ProfileStatus = [string]"Corrupted Local Profile"}
-            12 {$temp.ProfileStatus = [string]"Corrupted Roaming Profile"}
-            Default {$temp.ProfileStatus = [string]"Local"}
-        }
-        $res += $temp
-    }
-    return $res
-}
-
 function user_drives {
-Param(
-        [Parameter(Mandatory=$true, 
-                    ValueFromPipeline=$false,                    
-                    Position=0)]        
-	    $ary
+Param(     
+	    $ary,
+        $obj
     )
-    $res = @()
+    $num = 0
     foreach ($i in $ary) {
-        $temp = New-Object psobject -Property @{
-            DriveName = [String]$i.name
-            DriveFree = [String]"{0:N2} GB" -f ($i.freespace/1GB)
-            DriveTotal = [String]"{0:N2} GB" -f ($i.size/1GB)
-            DriveProvider = [String]$i.providername
-            DriveType = $i.drivetype
-        }
-        switch ($temp.drivetype) {
-            1 {$temp.drivetype = [string]"No Root Dir"}
-            2 {$temp.drivetype = [string]"Removable Disk"}
-            3 {$temp.drivetype = [string]"Logical Disk"}
-            4 {$temp.drivetype = [string]"Network Drive"}
-            5 {$temp.drivetype = [string]"CD drive"}
-            6 {$temp.drivetype = [string]"RAM disk"}
-            Default {$temp.drivetype = [string]"Unknown Type"}
-        }
-
-        $res += $temp
+        $dFree = [String]"{0:N2} GB" -f ($i.freespace/1GB)
+        $dTotal = [String]"{0:N2} GB" -f ($i.size/1GB)
+        switch ($i.drivetype) {
+            1 {$dType = [string]"No Root Dir"}
+            2 {$dType = [string]"Removable Disk"}
+            3 {$dType = [string]"Logical Disk"}
+            4 {$dType = [string]"Network Drive"}
+            5 {$dType = [string]"CD drive"}
+            6 {$dType = [string]"RAM disk"}
+            Default {$dType = [string]"Unknown Type"}        
+        } 
+        $obj | Add-Member -Name "Drive_$($num)_Name" -Value "$($i.name)" -MemberType NoteProperty
+        $obj | Add-Member -Name "Drive_$($num)_FreeSpace" -Value "$($dFree)" -MemberType NoteProperty
+        $obj | Add-Member -Name "Drive_$($num)_TotalSpace" -Value "$($dTotal)" -MemberType NoteProperty
+        $obj | Add-Member -Name "Drive_$($num)_Provider" -Value "$($i.providername)" -MemberType NoteProperty
+        $obj | Add-Member -Name "Drive_$($num)_Type" -Value "$($dType)" -MemberType NoteProperty
+        $num++
     }
-    return $res
+    return $obj
 }
 
 function computer_network {
-Param(
-        [Parameter(Mandatory=$true, 
-                    ValueFromPipeline=$false,                    
-                    Position=0)]        
-	    $ary
+Param(       
+	    $ary,
+        $obj
     )
-    
-    foreach ($i in $ary) {
-        $temp = New-Object psobject -Property @{
-            DHCP = [String]$i.dhcpenabled
-            NIC_Mac = [String]$i.MACAddress
-            Ip_addr_4 = [String[]]$i.ipaddress
-            Ip_addr_6 = [String]""
-            Gateway = [String]$i.DefaultIpGateway
-            DNS_server = [String]$i.DNSServerSearchOrder[0]
-            DNS_server2 = [String]$i.DNSServerSearchOrder[1]
-        }
+    $numb = 0
+    foreach ($i in $ary) {        
         #if there is more than 1 IP address do stuff
-        if ($temp.Ip_addr_4.length -gt 1) {
-            $temp.Ip_addr_6 = [String]$temp.Ip_addr_4[1]
-            $temp.Ip_addr_4 = [String]$temp.Ip_addr_4[0]
+        if ($i.Ipaddress.length -gt 1) {
+            $Ip_addr_6 = [String]$i.Ipaddress[1]
+            $Ip_addr_4 = [String]$i.Ipaddress[0]
         } else {
-            $temp.Ip_addr_4 = [String]$temp.Ip_addr_4
+            $Ip_addr_4 = [String]$i.Ipaddress
         }        
-
-        $res += $temp
+        $obj | Add-Member -Name "NIC_$($num)_Dhcp" -Value "$($i.dhcpenabled)" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_MAC_addr" -Value "$($i.MACAddress)" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_Ip_addr_4" -Value "$($Ip_addr_4)" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_Ip_addr_6" -Value "$($Ip_addr_6)" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_Gateway" -Value "$($i.DefaultIpGateway)" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_DNS_server_1" -Value "$($i.DNSServerSearchOrder[0])" -MemberType NoteProperty
+        $obj | Add-Member -Name "NIC_$($num)_DNS_server_2" -Value "$($i.DNSServerSearchOrder[1])" -MemberType NoteProperty
+        $num++
     }
-    return $res
+    return $obj
 }
 
 function get_system_info {
 Param(
         [Parameter(Mandatory=$false, 
                     ValueFromPipeline=$false,                    
-                    Position=0,
-                    ParameterSetName='Computers')]        
-	    [String[]]$computers = "Localhost" #cast this as a string array and fill by default
+                    Position=0)]        
+	    [String[]]$computers = $env:COMPUTERNAME #cast this as a string array and fill by default
     )
-    $results = @() #initalize array for results
-
     foreach ($c in $computers) {
         Write-Progress -Activity "Getting data" -Status "Working on $c" -PercentComplete ( ($c.Count/$computers.Length) * 100)
         #Get all the WMI data at once. This will probably take awhile.
@@ -250,12 +148,6 @@ Param(
         $drives = Get-WmiObject -Class win32_logicaldisk -ComputerName $c
         $network = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $c |`
             where{$_.IPEnabled -eq "True"}
-
-        #call functions
-        #$printerRes = user_printers($printers)
-        #$profileRes = user_profiles($profiles)
-        $drivesRes = user_drives($drives)
-        $networkRes = computer_network($network)
 
         $genInfo = New-Object PsObject -Property @{ #new object to Combine everything
             SystemName = [String]$system.Name
@@ -272,23 +164,12 @@ Param(
 
         user_printers -ary $printers -obj $genInfo
         user_profiles -ary $profiles -obj $genInfo
-        #the replacement:
-
-
-        #glob everything into a big array
-        #$results += $genInfo  
-        <#      
-        $results +=$drivesRes
-        $results += $profileRes
-        $results += $printerRes
-        $results += $networkRes
-        #>
+        user_drives -ary $drives -obj $genInfo
+        computer_network -ary $network -obj $genInfo        
         
         $domain = $env:USERDOMAIN
         $date = Get-Date -Format "MM-dd-yy_hhmm"
         #append to the csvFile
         $genInfo | Export-Csv "$($domain)_$($date).csv" -NoTypeInformation -Append
     }
-    
-    return $results
 }
