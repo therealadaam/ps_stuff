c0dcee9bd0df18c20d93d8446e4575b4fe679c50
Adam Rabenstein <adam.rabenstein@gmail.com>
2014-05-12 14:21:20 -0700

    Added get_shares and examples

---
+++ ip/get_shares.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Version
+    0.1
+    Created script, formated it out.
+
+.Synopsis
+   Created by Adam Rabenstein on 05-12-2014.
+   Gets a list of all the shares for localhost, options for remote computer, or input from list.
+   Exports to a .csv file
+  
+.EXAMPLE
+   get_shares
+
+.Todo
+    Get permissions on the shares
+
+#>
+
+#Get for localhost only
+Get-WmiObject -Class win32_share | select name,path,description | Export-CSV -NoTypeInformation 'shares.csv'
