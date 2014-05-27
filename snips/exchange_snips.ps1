5677aadea749b1697286a3f30ee68ac77562c20c
Adam Rabenstein <adam.rabenstein@gmail.com>
2014-05-27 15:34:55 -0700

    Adding 'snips' folder

---
+++ snips/exchange_snips.ps1
@@ -0,0 +1,8 @@
+﻿#region Connect to Exchange server in LAN or through VPN.
+
+#Used to connect to Exchange server so cmdlets can be run in any powershell session
+#change $server to the FQDN for the server hosting exchange
+$server = "server.domain.com"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$server/PowerShell/ -Authentication Kerberos
+Import-PSSession $Session
+#endregion
